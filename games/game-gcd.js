7a595a0ea5d9e4b5138ed3a5f720807bc8b89a6d
dnt-wrd <vladkrivenko1995@yandex.ru>
2021-05-15 15:11:32 +0300

    fix
    https://github.com/dnt-wrd/frontend-project-lvl1/runs/2589734398?check_suite_focus=true

--- games/game-gcd.js
+++ games/game-gcd.js
@@ -1,36 +1,41 @@
-import readlineSync from 'readline-sync';
 import {
-  getRandomInt, getName, countAndText, wrongAns, checkAns,
+  getRandomInt, getName, newCountAndText, checkAns,
 } from '../src/index.js';
 
 const gameName = 'calc';
-let a; let b; let
-  q;
-function brainGSD() {
-  const name = getName(gameName); // для комита
-
-  for (q = 1; q <= 3; q++) {
-    a = getRandomInt();
-    b = getRandomInt();
-    console.log(`Question: ${a} ${b}`);
-    // console.log("OTVET======> " + gcd(a, b));
-    const bool = checkAns(gcd(a, b), name);
-    if (!bool) {
-      q = 4;
-    }
-  }
-  if (q = 3) {
-    countAndText(q, name);
-  }
-}
+let a;
+let b;
+// let q;
 
 function gcd(a, b) {
   let i = a;
   while (a % i !== 0 || b % i !== 0) {
     // console.log(a % i);
-    i--;
+    i -= 1;
   }
   return i;
+}
+
+function brainGSD() {
+  const name = getName(gameName); // для комита
+  let i1 = 0;
+  let checkLoop = true;
+  while (checkLoop) {
+    a = getRandomInt();
+    b = getRandomInt();
+    console.log(`Question: ${a} ${b}`);
+    const checkLoopArray = checkAns(gcd(a, b), name);
+    checkLoop = checkLoopArray;
+    if (checkLoop) {
+      i1 += 1;
+    }
+    if (i1 === 3) {
+      checkLoop = false;
+    }
+  }
+  if (i1 === 3) {
+    newCountAndText(name);
+  }
 }
 
 // brainGSD();
